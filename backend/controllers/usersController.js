bead9f1e5c145022769a9bf9758e02aafb1bb486
Ofir <ofir.duchovne@gmail.com>
2021-12-20 10:04:14 +0200

    users controller init

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,9 @@
+const getAllUsers = (req, res) => {}
+
+const getUserById = (req, res) => {}
+
+const createUser = (req, res) => {}
+
+const updateUser = (req, res) => {}
+
+const deleteUser = (req, res) => {}
