a8490d29b740ca73e87cef20ee29221e0b6b9cb6
Ofir <ofir.duchovne@gmail.com>
2021-12-21 12:00:09 +0200

    matches controller done

--- backend/controllers/matchesController.js
+++ backend/controllers/matchesController.js
@@ -1,11 +1,61 @@
+const Match = require('../models/matchModel');
 
-module.exports = {
-
-    getAllMatchesOfUser: (req, res) => {},    
-    getMatch: (req, res) => {},
-    addNewMatch: (req, res) => {},
-    updateMatch: (req, res) => {},
-    deleteMatch: (req, res) => {},
+const matchesController = {
+    getAllMatchesOfUser: (req, res) => {
+        const userId = req.params.userId;
+        Match.find({ $or: [{ firstUser: userId }, { secondUser: userId }] }, (err, matches) => {
+            if (err) {
+                res.json({ status: false, message: err });
+            } else {
+                res.json({ status: true, data: matches });
+            }
+        }
+        );
+    },    
+    getMatch: (req, res) => {
+        const matchId = req.params.matchId;
+        Match.findById(matchId, (err, match) => {
+            if (err) {
+                res.json({ status: false, message: err });
+            } else {
+                res.json({ status: true, data: match });
+            }
+        }
+        );
+    },
+    addNewMatch: (req, res) => {
+        const match = new Match(req.body);
+        match.save((err, match) => {
+            if (err) {
+                res.json({ status: false, message: err });
+            } else {
+                res.json({ status: true, data: match });
+            }
+        });
+    },
+    updateMatch: (req, res) => {
+        const matchId = req.params.matchId;
+        Match.findByIdAndUpdate(matchId, req.body, (err, match) => {
+            if (err) {
+                res.json({ status: false, message: err });
+            } else {
+                res.json({ status: true, data: match });
+            }
+        });
+    },
+    deleteMatch: (req, res) => {
+        const matchId = req.params.matchId;
+        Match.findByIdAndRemove(matchId, (err, match) => {
+            if (err) {
+                res.json({ status: false, message: err });
+            } else {
+                res.json({ status: true, data: match });
+            }
+        });
+    },
 }
+
+
+module.exports = matchesController;
 
 
