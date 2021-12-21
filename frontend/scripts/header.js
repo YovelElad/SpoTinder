02cec40b08f6a0676ca664c8ee46ce60683eb9d5
Ofir <ofir.duchovne@gmail.com>
2021-12-21 20:30:09 +0200

    my matches - under construction

--- frontend/scripts/header.js
+++ frontend/scripts/header.js
@@ -1,9 +1,9 @@
 $("#nav-home").click(function() {
-    window.location.href = "/list.html?id=" + urlParams.get("id");
+    window.location.href = "/frontend/list.html?id=" + urlParams.get("id");
 });
 $("#nav-profile").click(function() {
-    window.location.href = "/personalDetails.html?id=" + urlParams.get("id");
+    window.location.href = "/frontend/personalDetails.html?id=" + urlParams.get("id");
 });
 $("#nav-myMatches").click(function() {
-    window.location.href = "/myMatches.html?id=" + urlParams.get("id");
+    window.location.href = "/frontend/myMatches.html?id=" + urlParams.get("id");
 });
