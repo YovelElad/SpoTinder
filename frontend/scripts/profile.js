c085f47ccab572725eeb5865f80989dd8a24fb66
Ofir <ofir.duchovne@gmail.com>
2021-12-22 17:23:47 +0200

    added new profile page

---
+++ frontend/scripts/profile.js
@@ -0,0 +1,78 @@
+const userId = urlParams.get('id');
+let user = null;
+
+function buildProfile(user) {
+    $("#profilePic").attr("src", user.image);
+    $("#profileName").text(user.name);
+    $("#profileEmail").text(user.email);
+    $("input[name='name']").val(user.name);
+    $("input[name='email']").val(user.email);
+    $("input[name='password']").val(user.password);
+    $("input[name='country']").val(user.country);
+    if(user.gender == "male") {
+        $("#maleRadio").prop("checked", true);
+    } else {
+        $("#femaleRadio").prop("checked", true);
+    }
+    if(user.interestedIn.includes("male")) {
+        $("#maleCheckBox").prop("checked", true);
+    }
+    if(user.interestedIn.includes("female")) {
+        $("#femaleCheckBox").prop("checked", true);
+    }
+}
+
+
+$("#profileForm").submit(function(e) {
+    e.preventDefault();
+    let preferences = [];
+    $.each($("input[name='genderPreference']:checked"), function() {
+        preferences.push($(this).val());
+    });
+    user.name = $("input[name='name']").val();
+    user.email = $("input[name='email']").val();
+    user.password = $("input[name='password']").val();
+    user.country = $("input[name='country']").val();
+    user.interestedIn = preferences;
+    if($("#maleRadio").is(":checked")) {
+        user.gender = "male";
+    } else {
+        user.gender = "female";
+    }
+
+    $.ajax({
+        url: `${API_URL}/users/${userId}`,
+        type: 'PUT',
+        data: user,
+
+        success: function(data) {
+            console.log(data);
+            if (data.status) {
+                console.log(data.data);
+                buildProfile(user);
+                // window.location.href = "/frontend/spotifyLogin.html?id=" + data.data._id;
+            } else {
+                alert(data.message);
+                // window.location.href = "/frontend/login.html";
+            }
+
+        }
+    });
+});
+
+
+$(document).ready(async() => {
+    $.ajax({
+        url: `${API_URL}/users/${userId}`,
+        type: "GET",
+        success: function(data) {
+            if(data.status) {
+                user = data.data;
+                buildProfile(user);
+            } else {
+                alert(data.message);
+            }
+        },
+        dataType: "JSON"
+    });
+});
