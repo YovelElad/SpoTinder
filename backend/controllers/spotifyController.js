21f77d35b6128f9ce064a305ff70ccaa9d33aedd
Ofir <ofir.duchovne@gmail.com>
2021-12-22 18:31:59 +0200

    clean + start script

--- backend/controllers/spotifyController.js
+++ backend/controllers/spotifyController.js
@@ -1,53 +1,52 @@
-const api = require('../modules/spotifyAPI');
-const User = require("../models/userModel");
+const api = require("../modules/spotifyAPI");
 const Match = require("../models/matchModel");
-const matchEngine = require('../modules/matching');
-const DB = require('../data/index');
+const matchEngine = require("../modules/matching");
+const DB = require("../data/index");
 
 callback = (req, res) => {
-        const code = req.query.code || null;
-        console.log(req.query);
-        if (code) {
-            const userId = req.query.state;
-            api.authorizeSpotify(userId, code).then(user => {
-                api.buildUserProfile(user).then((newUser) => {
-                    console.log(`then of buildUserProfile, user: ${newUser}`);
-                    DB.getUser(userId).then((user) => {
-                        console.log(`then of getUser, user: ${user.id}`);
-                        if (user) {
-                            newUser.email = user.email;
-                            newUser.password = user.password;
-                            newUser.gender = user.gender;
-                            newUser.interestedIn = user.interestedIn;
-                        }
-                        DB.updateUser(userId, newUser);
-                        matchEngine.calculateMatches(newUser).then((matches) => {
-                            console.log(`then of calculateMatches, user: ${matches}`);
-                            Match.insertMany(matches, (err, docs) => {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    console.log(`inserted ${docs.length} matches`);
-                                }
-                            });
-                            res.redirect('/list.html?id=' + userId);
-                        });
-                    })
-                })
+  const code = req.query.code || null;
+  console.log(req.query);
+  if (code) {
+    const userId = req.query.state;
+    api.authorizeSpotify(userId, code).then((user) => {
+      api.buildUserProfile(user).then((newUser) => {
+        console.log(`then of buildUserProfile, user: ${newUser}`);
+        DB.getUser(userId).then((user) => {
+          console.log(`then of getUser, user: ${user.id}`);
+          if (user) {
+            newUser.email = user.email;
+            newUser.password = user.password;
+            newUser.gender = user.gender;
+            newUser.interestedIn = user.interestedIn;
+          }
+          DB.updateUser(userId, newUser);
+          matchEngine.calculateMatches(newUser).then((matches) => {
+            console.log(`then of calculateMatches, user: ${matches}`);
+            Match.insertMany(matches, (err, docs) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(`inserted ${docs.length} matches`);
+              }
             });
-        } else {
-            console.log(`error: ${req.query}`);
-            res.json({ status: false, message: 'Error logging in to spotify' });
-        }
-    } 
+            res.redirect("/list.html?id=" + userId);
+          });
+        });
+      });
+    });
+  } else {
+    console.log(`error: ${req.query}`);
+    res.json({ status: false, message: "Error logging in to spotify" });
+  }
+};
 
 login = (req, res) => {
-    const userId = req.params.userId;
-    const url = api.getAuthorizationUrl(userId);
-    res.json({ status: true, data: url });
-}
+  const userId = req.params.userId;
+  const url = api.getAuthorizationUrl(userId);
+  res.json({ status: true, data: url });
+};
 
 module.exports = {
-    callback,
-    login
-}
+  callback,
+  login,
+};
--- backend/data/index.js
+++ backend/data/index.js
@@ -1,73 +1,68 @@
 const User = require("../models/userModel");
 
-
 module.exports = {
-    addUser: function(user) {
-        const newUser = new User(user);
-        newUser.save((err, user) => {
-            if (err) {
-                throw new Error(err);
-            } 
-        });
-    },
+  addUser: function (user) {
+    const newUser = new User(user);
+    newUser.save((err, user) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+  getUser: function (id) {
+    return new Promise((resolve, reject) => {
+      User.findById(id, (err, user) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(user);
+        }
+      });
+    });
+  },
 
-    getUser: function(id) {
-        return new Promise((resolve, reject) => {
-            User.findById(id, (err, user) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(user);
-                }
-            });
-        });
-    },
-
-    getUsers: function() {
-        return new Promise((resolve, reject) => {
-            User.find({}, (err, users) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(users);
-                }
-            });
-        });
-    },
-
-    updateUser: function(id, user) {
-        return new Promise((resolve, reject) => {
-            User.findByIdAndUpdate(id, user, (err, user) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(user);
-                }
-            });
-        });
-    },
-
-    setUserToken: function(id, token) {
-        return new Promise((resolve, reject) => {
-            User.findByIdAndUpdate(id, { token }, (err, user) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(user);
-                }
-            });
-        });
-    },
-    getUserToken: function(id) {
-        return new Promise((resolve, reject) => {
-            User.findById(id, (err, user) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(user.token);
-                }
-            });
-        });
-    },
+  getUsers: function () {
+    return new Promise((resolve, reject) => {
+      User.find({}, (err, users) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(users);
+        }
+      });
+    });
+  },
+  updateUser: function (id, user) {
+    return new Promise((resolve, reject) => {
+      User.findByIdAndUpdate(id, user, (err, user) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(user);
+        }
+      });
+    });
+  },
+  setUserToken: function (id, token) {
+    return new Promise((resolve, reject) => {
+      User.findByIdAndUpdate(id, { token }, (err, user) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(user);
+        }
+      });
+    });
+  },
+  getUserToken: function (id) {
+    return new Promise((resolve, reject) => {
+      User.findById(id, (err, user) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(user.token);
+        }
+      });
+    });
+  },
 };
-
