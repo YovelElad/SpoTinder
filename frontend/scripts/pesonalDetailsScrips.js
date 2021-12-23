fe60549ec27ff463125b852b768b3be8869e9740
Ofir <ofir.duchovne@gmail.com>
2021-12-23 07:14:55 +0200

    merge with yovel - match page

--- frontend/scripts/pesonalDetailsScrips.js
+++ frontend/scripts/pesonalDetailsScrips.js
@@ -1,61 +1,79 @@
-//Catch the user id from query
-// let id = 0;
-// const sPageURL = window.location.search.substring(1);
-// console.log(sPageURL);
-// let sURLVariables = sPageURL.split('&'),
-//     sParameterName = "",
-//     i = 0;
-// console.log(sURLVariables);
-// for (i = 0; i < sURLVariables.length; i++) {
-//     sParameterName = sURLVariables[i].split('=');
-//     console.log(sParameterName[1]);
-//     id = sParameterName[1];
-// }
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
 const userId = urlParams.get('id');
-console.log("id=" + userId);
+const urlUser = urlParams.get('user');
 
-// console.log(id);
+async function getScore(user) {
+    const responds = await fetch(`${API_URL}/users/${user._id}/matches`);
+    const matches = await responds.json();
+    console.log(matches);
+    const match = matches.data.find(m => m.firstUser == userId || m.secondUser == userId);
+    console.log(match.score);
+    return match.score;
+}
 
-function getMyIndex(data, id) {
-    for (leti = 0; i < data.length; i++) {
-        if (id == data[i]._id) {
-            return i;
-        }
-        console.log(i);
+function buildHeader(user) {
+    const title = document.createElement('h1');
+    const header = $("header");
+    title.innerHTML = `${user.name.split(" ")[0]}'s Profile`;
+    header.append('<br>');
+    header.append(title);
+    header.append('<br>');
+}
+
+async function buildProfile(user) {
+    const mainDetails = $("#personalDetails > #mainDetails");
+    const artists = $("#personalDetails > #moreDetails > #artists");
+    const tracks = $("#personalDetails > #moreDetails > #tracks");
+    const userName = document.createElement('h2');
+    userName.innerHTML = user.name;
+    const userImg = document.createElement('img');
+    userImg.src = user.image;
+    mainDetails.append(userName);
+    mainDetails.append(userImg);
+    const scoreTitle = document.createElement('p');
+    scoreTitle.id = "scoreTitle";
+    scoreTitle.innerHTML = "Your Score:";
+    mainDetails.append(scoreTitle);
+    const score = document.createElement('p');
+    score.id = "matchScore";
+    score.innerHTML = Math.round(await getScore(user) * 100) + "%";
+    mainDetails.append(score);
+    const artistsTitle = document.createElement('h6');
+    artistsTitle.innerHTML = `${user.name.split(" ")[0]}'s Top 5 Artists:`
+    artistsTitle.id = "artistsTitle";
+    artists.append(artistsTitle);
+    const artistsUL = document.createElement('ul');
+    artistsUL.id = "ulArtists";
+    for (let i = 0; i < 5; i++) {
+        const artistsLI = document.createElement('li');
+        artistsLI.innerHTML = user.topArtists[i];
+        artistsUL.append(artistsLI);
     }
+    artists.append(artistsUL);
+    const tracksTitle = document.createElement('h6');
+    tracksTitle.innerHTML = `${user.name.split(" ")[0]}'s Top 5 Tracks:`
+    tracksTitle.id = "tracksTitle";
+    tracks.append(tracksTitle);
+    const tracksUL = document.createElement('ul');
+    tracksUL.id = "ulArtists";
+    for (let i = 0; i < 5; i++) {
+        const tracksLI = document.createElement('li');
+        tracksLI.innerHTML = user.topTracks[i];
+        tracksUL.append(tracksLI);
+    }
+    tracks.append(tracksUL);
 }
 
 $.ajax({
     type: 'GET',
-    url: 'http://localhost:8888/users/' + userId,
+    url: `${API_URL}/users/` + urlUser,
     success: function(result) {
 
-        console.log(result);
-        $("#personalDetails > #mainDetails").append('<h1>' + result.data.name + '</h1>');
-        $("#personalDetails > #mainDetails").append('<img src="' + result.data.image + '">');
+        buildProfile(result.data);
+        buildHeader(result.data);
 
-
-        $("#personalDetails > #moreDetails > #artists").append('<h6 id="artistsTitle">Your Top 5 Artists:</h6>');
-        $("#personalDetails > #moreDetails > #artists").append('<ul id="ulArtists">');
-        for (let i = 0; i < 5; i++) {
-            console.log(result.data.topArtists[i]);
-            $("#personalDetails > #moreDetails > #artists > #ulArtists").append('<li>' + result.data.topArtists[i] + '</li>');
-        }
-        $("#personalDetails > #moreDetails > #artists").append('</ul><br>');
-
-
-
-        $("#personalDetails > #moreDetails > #tracks").append('<h6 id="tracksTitle">Your Top 5 Tracks:</h6>');
-        $("#personalDetails > #moreDetails > #tracks").append('<ul id="ulTracks">');
-        for (let i = 0; i < 5; i++) {
-            console.log(result.data.topTracks[i]);
-            $("#personalDetails > #moreDetails > #tracks > #ulTracks").append('<li>' + result.data.topTracks[i] + '</li>');
-        }
-        $("#personalDetails > #moreDetails > #tracks").append('</ul>');
     }
 })
+
 
 $("#homePageLink").click(function() {
     console.log("profile");
