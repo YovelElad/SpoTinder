a93a1f30b1ea9e0352e01fef1a8958cfbbe38378
Ofir <ofir.duchovne@gmail.com>
2022-01-19 18:22:27 +0200

    changed restriction mode

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const usersController = require('../controllers/usersController');
 const { matchesRouter } = require('../routers/matchesRouter');
+const auth  = require('../modules/auth.js');
 
 const usersRouter = new Router();
-usersRouter.use('/:userId/matches', matchesRouter);
+usersRouter.use('/:userId/matches',[auth.isPaid], matchesRouter);
 usersRouter.get('/', usersController.getAllUsers);
 usersRouter.get('/:userId', usersController.getUserById);
 usersRouter.post('/', usersController.createUser);
